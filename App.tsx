b56c1c682bbd8bb467e77937a628ec61bc32289d
k2 <izmail.k2@gmail.com>
2024-03-08 19:20:13 +0300

    docs: added aliases
    - Added alias @src for easier navigation.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {globalStyle} from "@/assets/style/globalStyle";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyle.main}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });

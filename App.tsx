e664d1f8b119290632838d3ec167a69ad13bedf6
k2 <izmail.k2@gmail.com>
2024-03-08 21:59:11 +0300

    feat: added font loading - Fonts are loaded before the app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
-import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import {useEffect, useState} from "react";
 import {globalStyle} from "@/assets/style/globalStyle";
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+const getFonts = () => Font.loadAsync({
+  'popsMedium': require('@/assets/fonts/Poppins-Medium.ttf'),
+  'popsRegular': require('@/assets/fonts/Poppins-Regular.ttf'),
+})
 
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false)//подключены ли шрифты
+
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+
+    getFonts()
+      .then(() => setFontLoaded(true))
+      .catch(console.warn)
+      .finally(() => SplashScreen.hideAsync())
+  }, []);
+
+  if (!fontLoaded) {
+    return null
+  }
+
   return (
     <View style={globalStyle.main}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto"/>
+      <Text>Hello!</Text>
     </View>
   );
 }
@@ -19,4 +40,7 @@ const styles = StyleSheet.create({
   //   alignItems: 'center',
   //   justifyContent: 'center',
   // },
+  // text:{
+  //   backgroundColor:'red'
+  // }
 });
